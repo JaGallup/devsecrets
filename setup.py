f2c621cc44e710ff31304c9668c02089d510e6e5
Haukur Hreinsson <haukur@ja.is>
2019-03-12 17:16:32 +0000

    Switch to MIT license, include license text

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Python Software Foundation License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
